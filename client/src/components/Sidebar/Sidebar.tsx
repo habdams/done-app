5f3bc8c784311f2f58216382554b61efcef81126
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2022-10-28 11:07:18 +0100

    Add done-app logo to sidebar

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -4,10 +4,12 @@ import { CheckSquare } from "phosphor-react";
 
 import "./Sidebar.scss";
 
+const logo: string = require("./images/logo.svg").default;
+
 export function Sidebar() {
   return (
     <section className="sidebar">
-      <img src="#" alt="logo for done app" className="sidebar-logo" />
+      <img src={logo} alt="logo for done app" className="sidebar-logo" />
       <p className="sidebar-sub">Track how much you do in a day</p>
 
       <span className="sidebar-point">
